b79e849cb0d0b78162f4ef3ea1fa42a607443bee
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2024-03-20 00:11:18 +0500

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import axios from 'axios'
-import React, { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 export default function Home() {
+  //variables
   const [messages, setMessages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [typing, setTyping] = useState(false);
+  const [recentConversationId, setRecentConversationId] = useState('');
+  
+  //references
+  const messageDiv = useRef();
 
-  // -X POST https://getcody.ai/api/v1/messages \
-  // -H "Authorization: Bearer $ACCESS_TOKEN" \
-  // -H "Content-Type: application/json" \
-  // -d '{"content":"string","conversation_id":"string"}'
 
 
   //fetch all messages
@@ -20,24 +23,32 @@ export default function Home() {
         }
       });
       setMessages(response.data.data);
+      setRecentConversationId(response.data.data)
+      return;
     } catch (error) {
-      console.log(error)
+      console.log(error);
+      setTyping(false)
     }
 
   }, [])
-
   // send message
-  const sendMessage = async() => {
+  const sendMessage = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.post('https://getcody.ai/api/v1/messages', {
-        headers:{
-          'Content-Type':'application/json',
-          'Authorization':'Bearer 895DxNd88qTCyBAh1X65pYI2s41lZZc9ZY7JCM1L13a16323'
+      setTyping(true)
+      const response = await axios.post('https://getcody.ai/api/v1/messages', { content: query, conversation_id: 'WjnegEGA3bwZ' }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 895DxNd88qTCyBAh1X65pYI2s41lZZc9ZY7JCM1L13a16323'
         }
       });
-      console.log(response.data)
+      setQuery('')
+      console.log(response.data);
+      fetchMessages();
+      setTyping(false)
+      return;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
@@ -54,22 +65,22 @@ export default function Home() {
       </p>
       <br />
       {/* Messages container  */}
-      <div className="containerMain border-purple-900 border-solid border-[5px] rounded-lg w-[50vw] h-[60vh] p-6 mx-auto bg-purple-300 flex flex-col justify-between">
-        <div className='justify-self-start flex flex-col'>
+      <div className="containerMain border-purple-900 border-solid border-[5px] rounded-lg w-[50vw] max-h-[80vh] h-[80vh] p-6 mx-auto bg-purple-300 flex flex-col justify-between overflow-hidden overflow-y-auto">
+        <div className='justify-self-start flex flex-col-reverse '>
 
           {
             messages.map((message, index) => {
-              return <div key={index} className={message.machine ? "server" : "client"}>
+              return <div key={index} ref={messageDiv} className={message.machine ? "server bg-purple-950 shadow-2xl text-white" : "client  bg-purple-500 shadow-2xl text-white"}>
                 {message.content}
               </div>
             })
           }
         </div>
         {/* Message Input */}
-        <div className="w-full p-2 border-purple-900 justify-self-end">
+        <div className="w-[50vw] p-2 border-purple-900 justify-self-end absolute mt-96 block bg-purple-400">
           <form className='flex justify-between w-full' onSubmit={sendMessage}>
-          <input type="text" className='border-purple-900 w-[40vw] p-4 rounded-md shadow-2xl' />
-          <input type="submit" value="Send" className='shadow-md p-4 bg-purple-700 w-44 rounded-lg hover:rounded-xl cursor-pointer mx-2 hover:scale-105 transition-all ease-in-out text-white hover:bg-purple-950'/>
+            <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} className='border-purple-900 w-[40vw] p-4 rounded-md shadow-2xl outline-none border-none' placeholder='Consult with me' />
+            <input type="submit" value="Send" className='shadow-md p-4 bg-purple-700 w-44 rounded-lg hover:rounded-xl cursor-pointer mx-2 hover:scale-105 transition-all ease-in-out text-white hover:bg-purple-950' />
           </form>
         </div>
       </div>
