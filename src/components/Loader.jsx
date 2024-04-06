2cd73eb0fccdc2880d2dd4063269fd934644d280
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2024-04-07 02:14:12 +0500

    -

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div>
+      <div className="rounded-[100px] w-16 h-16 border-t-[5px] border-purple-900 animate-spin bg-transparent"></div>
+    </div>
+  )
+}
